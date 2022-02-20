c1912547f3de33ee81a5faf2586ab2bc828ce421
magarciasu <100067671+magarciasu@users.noreply.github.com>
2022-02-20 13:26:13 -0500

    Instrucción de clase
    Primera sesión

---
+++ Ejercicio_1.py
@@ -0,0 +1,8 @@
+print(" *** Bienvenidos, vamos a validar como funciona una aplicación de control de versiones ***")
+print("*** A continuación podrá comprobar el resultado de diversas operaciones matemáticas ***")
+print(6//3)
+print(6//3.)
+print(6.//3)
+print(6.//3.)
+print("Segunda prueba de actualización del repositorio local a remoto")
+
